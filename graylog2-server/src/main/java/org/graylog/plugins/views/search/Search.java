acf483ef3773c116a09b425e75d40a3d0f60cf8b
Konrad Merz <konrad@graylog.com>
2020-03-27 10:55:27 +0100

    Raise MissingStreamPermissionException for having no streams at all
    Prior to this change, if a user did not have permissions to any streams
    we raised a exception which would lead to an 404 error on the
    frontend.
    
    This change will raise an MissingStreamPermissionException which
    will mapped to a NOT_ACCETPABLE (406) error code which will be
    treated differently on the frontend side and not show a not found error.

--- graylog2-server/src/main/java/org/graylog/plugins/views/search/Search.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/search/Search.java
@@ -34,6 +34,7 @@ import org.graylog.plugins.views.search.views.PluginMetadataSummary;
 import org.graylog2.contentpacks.ContentPackable;
 import org.graylog2.contentpacks.EntityDescriptorIds;
 import org.graylog2.contentpacks.model.entities.SearchEntity;
+import org.graylog2.shared.rest.exceptions.MissingStreamPermissionException;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.mongojack.Id;
@@ -132,8 +133,10 @@ public abstract class Search implements ContentPackable<SearchEntity> {
 
         final ImmutableSet<String> defaultStreams = defaultStreamsSupplier.get();
 
-        if (defaultStreams.isEmpty())
-            throw new PermissionException("User doesn't have access to any streams");
+        if (defaultStreams.isEmpty()) {
+            throw new MissingStreamPermissionException("User doesn't have access to any streams",
+                    Collections.emptySet());
+        }
 
         final Set<Query> withDefaultStreams = withoutStreams.stream()
                 .map(q -> q.addStreamsToFilter(defaultStreams))
