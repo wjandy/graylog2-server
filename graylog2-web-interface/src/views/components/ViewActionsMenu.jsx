19740744cca3cce15ddae6c22058eed942bf3e4c
Linus Pahl <lpahl@protonmail.com>
2020-04-08 22:39:33 +0200

    Implement CurrentUserContext for ViewActionsMenu

--- graylog2-web-interface/src/views/components/ViewActionsMenu.jsx
+++ graylog2-web-interface/src/views/components/ViewActionsMenu.jsx
@@ -1,39 +1,40 @@
 // @flow strict
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { withRouter } from 'react-router';
 
-import { DropdownButton, MenuItem, Button, ButtonGroup } from 'components/graylog';
-import { Icon } from 'components/common';
-
 import connect from 'stores/connect';
-import StoreProvider from 'injection/StoreProvider';
 import PermissionsMixin from 'util/PermissionsMixin';
 import AppConfig from 'util/AppConfig';
 
 import onSaveView from 'views/logic/views/OnSaveViewAction';
 import onSaveAsView from 'views/logic/views/OnSaveAsViewAction';
-import DebugOverlay from 'views/components/DebugOverlay';
 import { ViewStore } from 'views/stores/ViewStore';
 import { SearchMetadataStore } from 'views/stores/SearchMetadataStore';
 import SearchMetadata from 'views/logic/search/SearchMetadata';
 import * as Permissions from 'views/Permissions';
 import View from 'views/logic/views/View';
-import ViewPropertiesModal from './views/ViewPropertiesModal';
+
+import { DropdownButton, MenuItem, Button, ButtonGroup } from 'components/graylog';
+import { Icon } from 'components/common';
+import CurrentUserContext from 'components/contexts/CurrentUserContext';
+import DebugOverlay from 'views/components/DebugOverlay';
 import ShareViewModal from './views/ShareViewModal';
+import ViewPropertiesModal from './views/ViewPropertiesModal';
 import IfDashboard from './dashboard/IfDashboard';
 import BigDisplayModeConfiguration from './dashboard/BigDisplayModeConfiguration';
 
-const CurrentUserStore = StoreProvider.getStore('CurrentUser');
 const { isPermitted } = PermissionsMixin;
 
-const _isAllowedToEdit = (view: View, currentUser) => isPermitted(currentUser.permissions, [Permissions.View.Edit(view.id)])
+// $FlowFixMe currentUser can't be null or undefined
+const _isAllowedToEdit = (view: View, currentUser = {}) => isPermitted(currentUser.permissions, [Permissions.View.Edit(view.id)])
   || (view.type === View.Type.Dashboard && isPermitted(currentUser.permissions, [`dashboards:edit:${view.id}`]));
 
 const _hasUndeclaredParameters = (searchMetadata: SearchMetadata) => searchMetadata.undeclared.size > 0;
 
-const ViewActionsMenu = ({ view, isNewView, metadata, currentUser, router }) => {
+const ViewActionsMenu = ({ view, isNewView, metadata, router }) => {
+  const currentUser = useContext(CurrentUserContext);
   const [shareViewOpen, setShareViewOpen] = useState(false);
   const [debugOpen, setDebugOpen] = useState(false);
   const [saveAsViewOpen, setSaveAsViewOpen] = useState(false);
@@ -94,11 +95,6 @@ const ViewActionsMenu = ({ view, isNewView, metadata, currentUser, router }) => 
 };
 
 ViewActionsMenu.propTypes = {
-  currentUser: PropTypes.shape({
-    currentUser: PropTypes.shape({
-      permissions: PropTypes.arrayOf(PropTypes.string),
-    }),
-  }),
   router: PropTypes.any.isRequired,
   metadata: PropTypes.shape({
     undeclared: ImmutablePropTypes.Set,
@@ -107,16 +103,8 @@ ViewActionsMenu.propTypes = {
   isNewView: PropTypes.bool.isRequired,
 };
 
-ViewActionsMenu.defaultProps = {
-  currentUser: {
-    currentUser: {
-      permissions: [],
-    },
-  },
-};
-
 export default connect(
   withRouter(ViewActionsMenu),
-  { metadata: SearchMetadataStore, view: ViewStore, currentUser: CurrentUserStore },
-  ({ view: { view, isNew }, currentUser: { currentUser }, ...rest }) => ({ currentUser, view, isNewView: isNew, ...rest }),
+  { metadata: SearchMetadataStore, view: ViewStore },
+  ({ view: { view, isNew }, ...rest }) => ({ view, isNewView: isNew, ...rest }),
 );
